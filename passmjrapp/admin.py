b1b42f8e12cd88d1ab52c668e7332dacb9432e4e
Mrfad <fady.ghalayiny@outlook.com>
2020-04-22 09:47:37 +0300

    fixed activ navbar

--- passmjrapp/admin.py
+++ passmjrapp/admin.py
@@ -3,22 +3,27 @@ from .models import PasswordAccount
 from .forms import CreateUserForm, CreatePassForm, UpdatePassForm
 from import_export.admin import ImportExportModelAdmin
 
-
-
-
-
-class CreatePassAdmin(admin.ModelAdmin):
-    list_display = ['title',
-                  'username',
-                  'password',
-                  'url',
-                  'additional_notes']
+@admin.register(PasswordAccount)
+class ViewAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ['title', 'username', 'password', 'url', 'additional_notes']
     form = CreatePassForm
     list_filter = ['user']
     search_fields = ['title']
 
+# class CreatePassAdmin(admin.ModelAdmin):
+#     list_display = ['title',
+#                   'username',
+#                   'password',
+#                   'url',
+#                   'additional_notes']
+#     form = CreatePassForm
+#     list_filter = ['user']
+#     search_fields = ['title']
 
-@admin.register(PasswordAccount)
-class ViewAdmin(ImportExportModelAdmin, CreatePassAdmin):
-    pass
-# admin.site.register(PasswordAccount, CreatePassAdmin)
+# @admin.register(PasswordAccount)
+# class ViewAdmin(ImportExportModelAdmin, CreatePassAdmin):
+#     pass
+# # admin.site.register(PasswordAccount, CreatePassAdmin)
+
+
+
