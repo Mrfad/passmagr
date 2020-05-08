bf94fdf1dc682e75c139532038380af5f9ee6ad2
Mrfad <fady.ghalayiny@outlook.com>
2020-05-08 20:22:46 +0300

    little modifications including new jquery table

--- passmjrapp/models.py
+++ passmjrapp/models.py
@@ -10,7 +10,7 @@ class PasswordAccount(models.Model):
     url = models.CharField(max_length=100, blank=True, null=True)
     additional_notes = models.TextField(blank=True,default=None, null=True)
     creation_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    last_modified = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    last_modified = models.DateTimeField(auto_now=True, blank=True, null=True)
     user = models.ForeignKey(User,default=None, on_delete=models.CASCADE)
 
     def __str__(self):
